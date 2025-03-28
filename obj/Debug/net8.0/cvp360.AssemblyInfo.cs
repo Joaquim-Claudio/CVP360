0e51cdfe803d10a244990bb0612cd5712e832411
Joaquim <joaquim.claudio2003@gmail.com>
2025-03-28 12:36:19 +0000

    Add .gitignore

--- obj/Debug/net8.0/cvp360.AssemblyInfo.cs
+++ obj/Debug/net8.0/cvp360.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("cvp360")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+36160a78eabc3aebdace935cc13b7f1df87df99c")]
 [assembly: System.Reflection.AssemblyProductAttribute("cvp360")]
 [assembly: System.Reflection.AssemblyTitleAttribute("cvp360")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
