d7d386fcc1bc47c630aae81814eb8f417d35b0f5
Edja da Silva <tamaramonteiro2014@icloud.com>
2025-03-29 00:22:08 +0000

    db

--- docker/sql/initial.sql
+++ docker/sql/initial.sql
@@ -1,60 +1,61 @@
 /*CREATES ALL THE TABLES*/
 CREATE TABLE IF NOT EXISTS Profile (
-    ID SERIAL PRIMARY KEY,
+    Id SERIAL PRIMARY KEY,
     Name TEXT NOT NULL,
     ExtendedName TEXT
 );
 
 CREATE TABLE IF NOT EXISTS Page (
-    ID SERIAL PRIMARY KEY,
+    Id SERIAL PRIMARY KEY,
     Name TEXT NOT NULL,
-    URL TEXT
+    Url TEXT
 );
 
 CREATE TABLE IF NOT EXISTS ProfilePage (
-    ProfileID INTEGER REFERENCES Profile(ID) ON UPDATE CASCADE ON DELETE CASCADE,
-    PageID INTEGER REFERENCES Page(ID) ON UPDATE CASCADE ON DELETE CASCADE,
-    CONSTRAINT profile_page_key PRIMARY KEY (ProfileID, PageID)
+    ProfileId INTEGER REFERENCES Profile(Id) ON UPDATE CASCADE ON DELETE CASCADE,
+    PageId INTEGER REFERENCES Page(Id) ON UPDATE CASCADE ON DELETE CASCADE,
+    CONSTRAINT profile_page_key PRIMARY KEY (ProfileId, PageId)
 );
 
 CREATE TABLE IF NOT EXISTS CreditCard (
-    ID SERIAL PRIMARY KEY,
+    Id SERIAL PRIMARY KEY,
     Number TEXT NOT NULL,
     Name TEXT NOT NULL,
     ExpireDate DATE NOT NULL,
-    CVV VARCHAR(3) NOT NULL
+    Cvv VARCHAR(3) NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS Users (
-    ID SERIAL PRIMARY KEY,
+    Id SERIAL PRIMARY KEY,
     FullName TEXT NOT NULL,
     BirthDate DATE,
     PhoneNumber TEXT,
-    NIF TEXT,
-    ProfileID INTEGER REFERENCES Profile(ID) ON UPDATE CASCADE ON DELETE CASCADE ,
-    CreditCardID INTEGER REFERENCES CreditCard(ID) ON UPDATE CASCADE ON DELETE SET NULL
+    Nif TEXT,
+    ProfileId INTEGER REFERENCES Profile(Id) ON UPDATE CASCADE ON DELETE CASCADE ,
+    CreditCardId INTEGER REFERENCES CreditCard(Id) ON UPDATE CASCADE ON DELETE SET NULL
 );
 
 CREATE TABLE IF NOT EXISTS Project (
-    ID SERIAL PRIMARY KEY,
+    Id SERIAL PRIMARY KEY,
     Title TEXT NOT NULL,
     Description TEXT NOT NULL,
     StartDate DATE,
     EndDate DATE,
     ActionText TEXT,
-    ImgURL TEXT
+    ImgUrl TEXT
 );
 
 CREATE TABLE IF NOT EXISTS EnrollmentStatus (
-    ID SERIAL PRIMARY KEY,
+    Id SERIAL PRIMARY KEY,
     Status TEXT NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS Enrollment (
-    ID SERIAL PRIMARY KEY,
+    Id SERIAL PRIMARY KEY,
     Date DATE NOT NULL DEFAULT CURRENT_DATE,
     Value DECIMAL NOT NULL,
-    UserID INTEGER REFERENCES Users(ID) ON UPDATE CASCADE ON DELETE CASCADE
+    EnStatusId INTEGER REFERENCES EnrollmentStatus(Id) ON UPDATE CASCADE ON DELETE SET NULL,
+    UserId INTEGER REFERENCES Users(Id) ON UPDATE CASCADE ON DELETE CASCADE
 );
 
 /*INSERTS TEST DATA TO THE TABLES*/
@@ -64,8 +65,47 @@ VALUES ('Admin', 'Administrador'),
        ('Donor', 'Doador'),
        ('Editor', 'Editor de Conteúdo');
 
+INSERT INTO page(Name, Url)
+VALUES('CVP360', '/'),
+      ('Login', '/Login'),
+      ('Register', '/Register'),
+      ('Homepage', '/Homepage'),
+      ('Donations', '/Donations'),
+      ('Profile', '/Profile'),
+      ('Control Panel', '/ControlPanel');
 
-INSERT INTO Users (FullName, BirthDate, PhoneNumber, NIF, ProfileID)
-VALUES ('Diogo Pinheiro', '1996-06-15', '922369221', '305689444', (SELECT ID FROM Profile WHERE Name = 'Admin')),
-       ('João Pedro Júnior', '1977-05-22', '935874441', '225874136', (SELECT ID FROM Profile WHERE Name = 'Donor')),
-       ('Mário George Morais de Oliveira Igreja', '2004-04-13', '932555878', '298874441', (SELECT ID FROM Profile WHERE Name = 'Editor'));
+INSERT INTO profilepage(ProfileId, PageId)
+VALUES((SELECT Id FROM Profile WHERE Name = 'Admin'), (SELECT name FROM page WHERE Name ='CVP360') ),
+      ((SELECT Id FROM Profile WHERE Name = 'Admin'), (SELECT name FROM page WHERE Name ='Login')),
+      ((SELECT Id FROM Profile WHERE Name = 'Admin'), (SELECT name FROM page WHERE Name ='Register')),
+      ((SELECT Id FROM Profile WHERE Name = 'Admin'), (SELECT name FROM page WHERE Name ='Homepage')),
+      ((SELECT Id FROM Profile WHERE Name = 'Admin'), (SELECT name FROM page WHERE Name ='Donations')),
+      ((SELECT Id FROM Profile WHERE Name = 'Admin'), (SELECT name FROM page WHERE Name ='Profile')),
+      ((SELECT Id FROM Profile WHERE Name = 'Admin'), (SELECT name FROM page WHERE Name ='Control Panel')),
+
+      ((SELECT Id FROM Profile WHERE Name = 'Donor'), (SELECT name FROM page WHERE Name ='CVP360') ),
+      ((SELECT Id FROM Profile WHERE Name = 'Donor'), (SELECT name FROM page WHERE Name ='Login')),
+      ((SELECT Id FROM Profile WHERE Name = 'Donor'), (SELECT name FROM page WHERE Name ='Register')),
+      ((SELECT Id FROM Profile WHERE Name = 'Donor'), (SELECT name FROM page WHERE Name ='Homepage')),
+      ((SELECT Id FROM Profile WHERE Name = 'Donor'), (SELECT name FROM page WHERE Name ='Donations')),
+      ((SELECT Id FROM Profile WHERE Name = 'Donor'), (SELECT name FROM page WHERE Name ='Profile')),
+
+      ((SELECT Id FROM Profile WHERE Name = 'Editor'), (SELECT name FROM page WHERE Name ='Control Panel'));
+
+
+
+INSERT INTO Users (FullName, BirthDate, PhoneNumber, Nif, ProfileId)
+VALUES ('Diogo Pinheiro', '1996-06-15', '922369221', '305689444', (SELECT Id FROM Profile WHERE Name = 'Admin')),
+       ('João Pedro Júnior', '1977-05-22', '935874441', '225874136', (SELECT Id FROM Profile WHERE Name = 'Donor')),
+       ('Mário George Morais de Oliveira Igreja', '2004-04-13', '932555878', '298874441', (SELECT Id FROM Profile WHERE Name = 'Editor'));
+/*
+INSERT INTO enrollmentstatus()
+VALUES
+
+INSERT INTO enrollment()
+VALUES
+
+
+INSERT INTO project()
+VALUES
+ */
